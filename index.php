e785e7a9dcc9bba3062944d786d71cb327de9b71
Bryan Low <bryanlow987@gmail.com>
2018-04-25 18:42:57 +0800

    initialise git and created the outline with side navbar menu

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php ?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link href="css/sidebar_style.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+        <meta charset="UTF-8">
+        <title>Pomefresh</title>
+
+    </head>
+    <body>
+        <div id="mySidenav" class="sidenav">
+
+            <a href="" class="navlinks" onclick="showHomePage()">Home</a><br>
+            <a href="/Pomefresh/content/products" class="navlinks"">Products</a><br>
+            <a href="/Pomefresh/content/orders" class="navlinks">Orders</a><br>
+            <a href="/Pomefresh/content/users" class="navlinks">Users</a><br>
+
+            <a href="#" id="login">Login</a>
+        </div>
+        <div id="page">
+            <div class="w3-card" id="header">
+                <h1>Pomefresh</h1>
+            </div>
+            
+        </div>
+        <script>
+          
+        </script>
+        
+    </body>
+</html>
